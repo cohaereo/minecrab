a2b6f0db57cbb743fa29d17b05cd50d10463d551
cohaereo <cohaereo@protonmail.com>
2022-11-21 22:26:32 +0100

    chunk_mesher: Add AO for down faces

--- src/render/chunk_mesher.rs
+++ src/render/chunk_mesher.rs
@@ -2,87 +2,11 @@ use cgmath::Vector3;
 
 use crate::world::{ChunkManager, ChunkSectionData};
 
-pub const MAX_VERTICES_PER_CHUNK: usize = 17 * 17 * 257;
-
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct ChunkVertex {
-    // pub pos: [f32; 3],
-    // pub pos: u32,
-    // pub block: u32,
     pub data: u32,
 }
-
-// /// All of these chunks are relative to the center chunk.
-// pub struct ChunkNeighbors<'c> {
-//     // ? Do we pass this as well?
-//     pub center: &'c ChunkSectionData,
-//     pub center_y: u8,
-
-//     pub up: Option<&'c ChunkSectionData>,
-//     pub down: Option<&'c ChunkSectionData>,
-//     pub north: Option<&'c ChunkSectionData>,
-//     pub east: Option<&'c ChunkSectionData>,
-//     pub south: Option<&'c ChunkSectionData>,
-//     pub west: Option<&'c ChunkSectionData>,
-// }
-
-// impl<'a> ChunkNeighbors<'a> {
-//     // TODO: Should we add a check for corners?
-//     pub fn get_block(&self, x: i32, y: i32, z: i32) -> u8 {
-//         // Is in column?
-//         let cd = if (0..16).contains(&x) && (0..16).contains(&z) {
-//             if y < 0 {
-//                 self.down
-//             } else if y >= 16 {
-//                 self.up
-//             } else {
-//                 Some(self.center)
-//             }
-//         }
-//         // It's in the 4 surrounding chunks
-//         else {
-//             if x < 0 {
-//                 self.west
-//             } else if x >= 16 {
-//                 self.east
-//             } else if z < 0 {
-//                 self.south
-//             } else if z >= 16 {
-//                 self.north
-//             } else {
-//                 unreachable!()
-//             }
-//         };
-
-//         cd.map_or(0, |c| c.get_block(x as i64, y as i64, z as i64))
-//     }
-
-//     /// Returns (up, down, left, right, front, back)
-//     pub fn get_neighbours(&self, x: i32, y: i32, z: i32) -> (bool, bool, bool, bool, bool, bool) {
-//         macro_rules! nb {
-//             ($x:expr, $y:expr, $z:expr) => {{
-//                 let (tx, ty, tz) = (x + $x, y + $y, z + $z);
-//                 self.get_block(tx, ty, tz) != 0
-//             }};
-//         }
-
-//         (
-//             nb!(0, 1, 0),  // Up
-//             nb!(0, -1, 0), // Down
-//             nb!(-1, 0, 0), // Left
-//             nb!(1, 0, 0),  // Right
-//             nb!(0, 0, -1), // Front
-//             nb!(0, 0, 1),  // Back
-//         )
-//     }
-
-//     pub fn is_block_obstructed(&self, x: i32, y: i32, z: i32) -> bool {
-//         let (n0, n1, n2, n3, n4, n5) = self.get_neighbours(x, y, z);
-
-//         n0 && n1 && n2 && n3 && n4 && n5
-//     }
-// }
 
 impl ChunkVertex {
     const ATTRIBS: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![0 => Uint32, 1 => Uint32];
@@ -223,16 +147,9 @@ pub fn mesh_chunk(
                 if block != 0 {
                     let (nup, ndown, nleft, nright, nfront, nback) =
                         cm.get_neighbors(base.x + x, base.y + y, base.z + z);
-                    // get_neighbors!(base.x + x, base.y + y, base.z + z);
-
-                    // let (nup, ndown, nleft, nright, nfront, nback) =
-                    //     (false, false, false, false, false, false);
 
                     let light = 0xf;
-                    // let light = cm.get_block_light((base.x + x, base.y + y, base.z + z));
-                    // .max(4);
 
-                    // * This code is a little more hefty because it creates 4 vertices for every face.
                     if !nup {
                         ao0 = calculate_ao!(
                             (x - 1, y + 1, z + 0),
@@ -267,31 +184,26 @@ pub fn mesh_chunk(
                     }
 
                     if !ndown {
-                        // ao0 = calculate_ao!(
-                        //     (x + 1, y - 1, z + 0),
-                        //     (x + 1, y - 1, z - 1),
-                        //     (x + 0, y - 1, z - 1)
-                        // );
-                        // ao1 = calculate_ao!(
-                        //     (x + 1, y - 1, z + 0),
-                        //     (x + 1, y - 1, z + 1),
-                        //     (x + 0, y - 1, z + 1)
-                        // );
-                        // ao2 = calculate_ao!(
-                        //     (x - 1, y - 1, z + 0),
-                        //     (x - 1, y - 1, z + 1),
-                        //     (x + 0, y - 1, z + 1)
-                        // );
-                        // ao3 = calculate_ao!(
-                        //     (x - 1, y - 1, z + 0),
-                        //     (x - 1, y - 1, z - 1),
-                        //     (x + 0, y - 1, z - 1)
-                        // );
-
-                        ao0 = 3;
-                        ao1 = 3;
-                        ao2 = 3;
-                        ao3 = 3;
+                        ao0 = calculate_ao!(
+                            (x + 1, y - 1, z + 0),
+                            (x + 1, y - 1, z - 1),
+                            (x + 0, y - 1, z - 1)
+                        );
+                        ao1 = calculate_ao!(
+                            (x + 1, y - 1, z + 0),
+                            (x + 1, y - 1, z + 1),
+                            (x + 0, y - 1, z + 1)
+                        );
+                        ao2 = calculate_ao!(
+                            (x - 1, y - 1, z + 0),
+                            (x - 1, y - 1, z + 1),
+                            (x + 0, y - 1, z + 1)
+                        );
+                        ao3 = calculate_ao!(
+                            (x - 1, y - 1, z + 0),
+                            (x - 1, y - 1, z - 1),
+                            (x + 0, y - 1, z - 1)
+                        );
 
                         independent_face!(
                             1,
